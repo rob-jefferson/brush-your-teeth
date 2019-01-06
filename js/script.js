80cf1ec891c768cee79fbf3f5b470c0f7874d3d6
Rob Jefferson <robjjdn15@gmail.com>
2019-01-06 14:49:30 +0000

    Change of interface including fixed controls and full screen video

--- js/script.js
+++ js/script.js
@@ -11,12 +11,19 @@ var myVideo = document.getElementById("toothbrush");
 
 function playPause() {
     if (myVideo.paused) {
-        document.getElementById("playpause").innerHTML = "&#9208;";
+        document.getElementById("playpause").innerHTML = "Pause";
         myVideo.play();
+        if (myVideo.requestFullscreen) {
+            myVideo.requestFullscreen({ navigationUI: "hide" })
+                .then({}).catch(err => {
+                    alert(`An error occurred while trying to switch into full-screen mode: ${err.message} (${err.name})`);
+              });
+              screen.orientation.lock("landscape-primary");
+        };
     }
 
     else {
-        document.getElementById("playpause").innerHTML = "&#9654;";
+        document.getElementById("playpause").innerHTML = "Play";
         myVideo.pause();
     }
 }
@@ -24,7 +31,7 @@ function playPause() {
 function restart() {
     myVideo.currentTime = 5;
     if (myVideo.paused) {
-        document.getElementById("playpause").innerHTML = "&#9208;";
+        document.getElementById("playpause").innerHTML = "Pause";
         myVideo.play();
     }
 }
